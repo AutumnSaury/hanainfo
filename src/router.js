6e3e39f6f0af3e0f818a5f90780dc4fb8344cb0a
秋サンマ <autumnsaury@outlook.com>
2022-11-29 17:58:24 +0800

    ✨feature: add guard

--- src/router.js
+++ src/router.js
@@ -1,14 +1,18 @@
 import Router from './utils/RouterUtil.js'
 import './views/FrameworkView.js'
+import './views/FrontpageView.js'
+import './views/ArticlesView.js'
+import './views/OthersView.js'
+import './views/AboutView.js'
 import './views/SignView.js'
 /**
  * 路由记录
- * @typedef {Object} Route 一条路由记录
+ * @typedef {Object} Route
  * @property {string?} title 页面标题
  * @property {string?} path 路径
  * @property {string?} componentName 组件名
  * @property {Route[]?} children 子路由
- * @property {string?} fullPath 完整路径
+ * @property {string} fullPath 完整路径
  * @property {string?} redirect 重定向路径
  */
 
@@ -33,6 +37,16 @@ const routes = [
         componentName: 'frontpage-view'
       },
       {
+        title: '文章',
+        path: 'articles',
+        componentName: 'articles-view'
+      },
+      {
+        title: '其他',
+        path: 'others',
+        componentName: 'others-view'
+      },
+      {
         title: '关于',
         path: 'about',
         componentName: 'about-view'
@@ -41,8 +55,33 @@ const routes = [
   }
 ]
 
+/**
+ * @return {boolean} 是否已登录
+ */
+function isAuthenticated () {
+  return true
+}
+
 window.$router = new Router(routes)
+/**
+ * @param {Route} from
+ * @param {Route} to
+ * @return {boolean | Route}
+ */
 window.$router.beforeEach = (from, to) => {
-  console.log(from, to)
+  // console.log(from, to)
+  if (to.fullPath === '/') {
+    return {
+      fullPath: '/main'
+    }
+  }
+
+  if (!isAuthenticated() && to.fullPath !== '/sign') {
+    alert('请先登录')
+    return {
+      fullPath: '/sign'
+    }
+  }
+
   return true
 }
